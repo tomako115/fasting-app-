89fb64571bf215090cf259ddd41666a80a6b5689
shibata-tomomi <shibata.tomomi27@gmail.com>
2026-06-29 12:16:37 +0900

    edge runtime追加

--- app/clients/[id]/page.tsx
+++ app/clients/[id]/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+export const runtime = 'edge';
 import { useEffect, useState, useCallback } from 'react';
 import { useParams, useRouter } from 'next/navigation';
 import Link from 'next/link';
